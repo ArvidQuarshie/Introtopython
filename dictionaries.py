8c69aa50bade5997ec31fb74c7436329769c29d9
ArvidQuarshie <arvidquarshie@gmail.com>
2016-04-30 22:25:13 +0300

    dictionary

---
+++ dictionaries.py
@@ -0,0 +1,14 @@
+Python 2.7.10 (default, Oct 14 2015, 16:09:02) 
+[GCC 5.2.1 20151010] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> stuff={'name':'Arvid','age':'34','height':'5 inches'}
+
+>>> print stuff['name']
+Arvid
+>>> print['age']
+['age']
+>>> print stuff ['age']
+34
+>>> print stuff ['height']
+5 inches
+>>> 
