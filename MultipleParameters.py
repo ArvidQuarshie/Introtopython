0a52816d212170041348fbd5dd5cf1bc95511359
ArvidQuarshie <arvidquarshie@gmail.com>
2016-05-06 17:36:08 +0300

    new files

--- MultipleParameters.py
+++ MultipleParameters.py
@@ -1,19 +1,22 @@
+#!usr/bin/pyhton
+
 #function that takes 2 strings 1 dictionary to n arguments
  #takes multiple parameters
 
 x=raw_input("name")
 y=raw_input("age")
+#this is the list
 z={"school":"Moringa"}
-
-def function MultipleParameters(x,y,z,*a):
+#function that takes more than three parameters
+def show(x,y,z,*extra):
  
-   def double age(y):
-       return y*2
-    for key in z :
-        if [key]=="school"
-        print "Moringa School"
+        def double age(y):
+        return y*2
+           for key in z :
+              if [key]=="school"
+                 print "Moringa School"
 
 
 
-MultipleParameters("Arvid",40,Moringa,female)
+show("Arvid",40,Moringa,female)
     
