1e7c13a5c6730d6eba672e5cd0d53ff7a27cc679
ArvidQuarshie <arvidquarshie@gmail.com>
2016-05-06 16:19:15 +0300

    added multipleparameters

---
+++ MultipleParameters.py
@@ -0,0 +1,19 @@
+#function that takes 2 strings 1 dictionary to n arguments
+ #takes multiple parameters
+
+x=raw_input("name")
+y=raw_input("age")
+z={"school":"Moringa"}
+
+def function MultipleParameters(x,y,z,*a):
+ 
+   def double age(y):
+       return y*2
+    for key in z :
+        if [key]=="school"
+        print "Moringa School"
+
+
+
+MultipleParameters("Arvid",40,Moringa,female)
+    
