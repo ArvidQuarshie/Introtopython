7dcf2e6602b11cef2903afb982c90c54b02a4477
ArvidQuarshie <arvidquarshie@gmail.com>
2016-05-07 18:21:31 +0300

    edits

--- html_templates.py
+++ html_templates.py
@@ -5,6 +5,11 @@ from flask import Flask,render-template
 app=Flask(__name__)
 
 @app.route('/profile/<name>')
-#app routes to the index page
-def index(name):
-    return render_template("profile.html",name=name) 
+#app routes to the profile page
+def profile(name):
+    return render_template("profile.html",name=name)
+
+
+#initializes the server
+if __name__=="__main__":
+    app.run()
