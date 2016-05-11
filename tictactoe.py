d8b95ff2128e1865bda07f74d5ad1ae9332fb6c3
ArvidQuarshie <arvidquarshie@gmail.com>
2016-05-11 19:37:27 +0300

    added tic tac toe game

---
+++ tictactoe.py
@@ -0,0 +1,14 @@
+#!usr/bin/python
+#python tic tac toe
+
+#the game board
+
+board=[0,1,2,
+       3,4,5,
+       6,7,8]
+
+def show():
+    print board[0]'|',board[1],'|',board[2]
+    print board[0]'|',board[1],'|',board[2]
+    print board[0]'|',board[1],'|',board[2]
+    print board[0]'|',board[1],'|',board[2]
