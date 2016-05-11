f278a079cdd0bd7afd8850792cf6dc768bd0f225
ArvidQuarshie <arvidquarshie@gmail.com>
2016-05-11 19:06:29 +0300

    hangman game

---
+++ hangman.py
@@ -0,0 +1,1 @@
+print "%s is a very %s person. They usually spend all their time %s "%(raw_input("please enter a name :") ,raw_input("please enter an adjective :"), raw_input("please enter an ing verb :"))
