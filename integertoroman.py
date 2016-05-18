10fdd105b69ab5987dcec4197dd188a1d85ce4f6
ArvidQuarshie <arvidquarshie@gmail.com>
2016-05-18 12:39:31 +0300

    integer to roman

---
+++ integertoroman.py
@@ -0,0 +1,21 @@
+class solution:
+
+    def romanToInt(self,s):
+        d=dict()
+        d['I']=1
+        d['V']=5
+        d['X']=10
+        d['L']=50
+        d['C']=100
+        d['D']=500
+        d['M']=1000
+
+        result=[d[s[-1]]]
+
+        for i in range(len(s) -2 ,-1,-1):
+            if d[s[i]]< d[s[i+1]]:
+                result-=d[s[i]]
+            else:
+                result +=d[s[i]]
+
+        return result
